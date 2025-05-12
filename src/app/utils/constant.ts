148cccbd8a5ecf2b984b9b8ccdb1c3a51f5ea81b
Ali <aliahmed60345@gmail.com>
2025-05-12 13:51:54 +0300

    added domain

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -1,3 +1,4 @@
 export const ARTICLE_PER_PAGE = 6;
 
-export const DOMAIN = "http://localhost:3000";
+export const DOMAIN =
+  "https://article-full-stack-app-next-git-main-alis-projects-1be8f768.vercel.app/";
