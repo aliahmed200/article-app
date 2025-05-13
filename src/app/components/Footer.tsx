c6165073837f287b161aab60e17eed478da84fc5
Ali <aliahmed60345@gmail.com>
2025-05-13 11:12:55 +0300

    edit navbar layaout

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 py-4 border-t-1 fixed bottom-0 left-0 right-0">
+    <div className="flex flex-col items-center justify-center gap-2 py-4 border-t-1 mt-8 fixed bottom-0 left-0 right-0">
       <div>Copyright 2025 Cloud Hosting</div>
       <div>Ali Ahmed</div>
     </div>
