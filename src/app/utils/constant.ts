11bce47d18d2805f856fa66810d56d9c1eaa5c1c
Ali <aliahmed60345@gmail.com>
2025-05-30 02:23:13 +0300

    editnodeenv

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -1,5 +1,5 @@
 export const ARTICLE_PER_PAGE = 6;
 
-export const DOMAIN = "https://article-full-stack-app-next.vercel.app/";
+// export const DOMAIN = "https://article-full-stack-app-next.vercel.app/";
 
-// export const DOMAIN = "http://localhost:3000";
+export const DOMAIN = "http://localhost:3000";
